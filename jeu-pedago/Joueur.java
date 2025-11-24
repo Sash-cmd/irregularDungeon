5236ca2c866455b06237b5d598cdaf7718c97b05
Jules Goddefroy <133168992+Pyrthon1@users.noreply.github.com>
2025-11-24 11:36:00 +0100

    Update Joueur.java
    Retirer les initialisation dans les types, car les profs seront surement contre

--- jeu-pedago/Joueur.java
+++ jeu-pedago/Joueur.java
@@ -1,9 +1,9 @@
 class Joueur {
-    int id = 0;
-    String nom = "Kévin Jourdain";
-    int level = 1;
-    int xp = 0;
-    int gold = 0;
+    int id;
+    String nom;
+    int level;
+    int xp;
+    int gold;
     int pvMax;
     int pv;
 }
