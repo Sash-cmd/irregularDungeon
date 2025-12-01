37a32cc64973bb3ea7f03d34693c3d8b0600aab0
Jules Goddefroy <133168992+Pyrthon1@users.noreply.github.com>
2025-12-01 12:04:32 +0100

    Add files via upload

---
+++ jeu-pedago/Item.java
@@ -0,0 +1,14 @@
+class Item{
+    int id;
+    String nom;
+    String description;
+    /*
+    ID des items :
+    0 => petite pottion de soin
+    1 => grande potion de soin
+    2 => petit potion de QI
+    3 => grande potion de QI
+    MAX 9 
+    */
+
+}
