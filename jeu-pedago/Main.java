da3fbb84ae8911468045fc43fa353fdda0ad2781
Jules Goddefroy <133168992+Pyrthon1@users.noreply.github.com>
2025-12-02 13:22:38 +0100

    Update Main.java

--- jeu-pedago/Main.java
+++ jeu-pedago/Main.java
@@ -11,26 +11,43 @@ class Main extends Program {
     final int MAX_ITEMS = 4;
     final int PV_MAX_JOUEUR = 3;
     final Verbe[] ALL_VERBES = allVerbes("verbes.csv");
+    final Item[] ALL_ITEMS = allItems("items.csv");
+    Joueur[] sauvegardes = readSauvegardes("sauvegardes.csv");
     Joueur joueurActuel;
+    String positionJoueur = "Main Menu"; //(ptet un enum ?) positions incluent : "Main Menu", "Crossroad", "Academie", "Boutique Verbes", "Boutiques Items", "Donjon", "Couloir", "Combat", "Coffre"
     
-    void algorithm(){println(toString(ALL_VERBES));
+    void algorithm(){
+        print("Quel nb de fichier afficher ? ");
+        int input = readInt();
+        println();
+        SetUpGame(input);
+        afficherSauvegarde(input);
+    }
+
+    void SetUpGame(int numSauvegarde){
+        joueurActuel = sauvegardes[numSauvegarde];
+        positionJoueur = "Main Menu";
+    }
+
+    String readInput(){
+        return readString();
     }
 
     //---------------/Fonctions de toString\---------------//
     String toString(String[] s){String result = "";for(int i=0;i<length(s);i++){result += s[i]+"; ";}result += "\n";return result;}
-    String toString(Verbe v){return "ID: "+v.id +" "+v.fr +" "+ v.bv +" "+ v.pr +" "+ v.pp +" "+ v.level;}
-    String toString(Verbe[] v){
-        String result = "";
-        for(int i=0;i<length(v);i++){
-            result += toString(v[i])+"; \n";
-            }
-        return result;
-    }
-    String toString(Joueur j){return "Nom: "+j.nom +" "+"niveau:"+j.level +" "+"xp: "+j.xp +" "+"gold: "+j.gold +" "+"pv: "+j.pv +"\n"+toString(j.livre)+"\n"+toString(j.inventaire);}
+    String toString(Verbe v){return "ID: "+v.id +" "+v.fr +" "+ v.bv +" "+ v.pr +" "+ v.pp +" lv"+ v.level;}
+    String toString(Verbe[] v){String result = "";for(int i=0;i<length(v);i++){result += toString(v[i])+";\n";}return result;}
+    String toString(Joueur j){return "Nom:"+j.nom +" "+"niveau:"+j.level +" "+"xp:"+j.xp +" "+"gold:"+j.gold +" "+"pv:"+j.pv +"\n\nVerbes :\n"+toString(j.livre)+"\nItems :\n"+toString(j.inventaire);}
     String toString(Monstre m){return "ID: "+m.id+" PvMax: "+m.pvMax+" Pv: "+m.pv +" Couleur: "+m.color+" "+COLORS[m.color]+" xpGiven: "+m.xpGiven+" goldGiven: "+m.goldGiven+" VERBE: "+toString(m.verbe);}
     String toString(Item i){return "ID: "+i.id+" Nom: "+i.nom+" Description: "+i.description;}
+    String toString(Item[] inv){String result="";for(int i=0;i<length(inv);i++){result+=toString(inv[i])+";\n";}return result;}
     //-----------------------------------------------------//
 
+    //-----------------/Fonctions de print\----------------//
+    void afficherSauvegarde(int numSauvegarde){println(toString(sauvegardes[numSauvegarde]));}
+    
+
+    //-----------------------------------------------------//
 
     //-------------------/Constructeurs\-------------------//
     Joueur newJoueur(String nom, int level, int xp, int gold, int pv){
@@ -40,7 +57,7 @@ class Main extends Program {
         j.gold=gold;j.pvMax = 3;
         j.pv = pv;
         j.livre = new Verbe[MAX_VERBES];
-        j.inventaire = new String[MAX_ITEMS];
+        j.inventaire = new Item[MAX_ITEMS];
         return j;
     }
     Verbe newVerbe(int id, String fr, String bv, String pr, String pp, int level){
@@ -96,20 +113,24 @@ class Main extends Program {
     //-----------------/Fonctions de CSV\------------------//
 
     //Fonctions pour lire le fichier de sauvegarde csv
-    /*
     Joueur[] readSauvegardes(String fichier){
         CSVFile f = loadCSV("csv/"+fichier);
         Joueur[] sauvegardes = new Joueur[rowCount(f)];
         int nbJoueurs = 0;
         for(int l = 1; l<rowCount(f); l++){
-            sauvegardes[l] = newJoueur(getCell(f, l, 0),getcell(f,l,1),getcell(f,l,2),getcell(f,l,3),getcell(f,l,4))
+            sauvegardes[l] = newJoueur(getCell(f, l, 0),
+            intFromString(getCell(f,l,1)),
+            intFromString(getCell(f,l,2)),
+            intFromString(getCell(f,l,3)),
+            intFromString(getCell(f,l,4)));
             sauvegardes[l].pvMax = PV_MAX_JOUEUR;
             sauvegardes[l].livre = newLivre(f,l);
             sauvegardes[l].inventaire = newInventaire(f,l);
             nbJoueurs += 1;
-        } 
+        }
+        return sauvegardes;
     }
-    */
+    
 
     //Fonction pour enregistrer les verbes d'un csv en tableau
     Verbe[] allVerbes(String file){
@@ -125,17 +146,18 @@ class Main extends Program {
         }
         return livre;
     }
-
-    Verbe[] newLivre(CSVFile f, int ligne){
-        String livreString = getCell(f,ligne,5);
+    //Fonctionpour avoir le tableau de verbe d'un joueur a partir de son csv
+    Verbe[] newLivre(CSVFile save, int ligne){
+        String livreString = getCell(save,ligne,5);
         int[] nombres = StringIntoInt(livreString);
         int nbVerbes = length(nombres);
         Verbe[] livre = new Verbe[nbVerbes];
         for(int i = 0; i<nbVerbes; i++){
-
+            livre[i] = ALL_VERBES[nombres[i]];
         }
         return livre;
     }
+    //Fonction String de chiffres en int: "123" => 123
     int intFromString(String input){
         int result = 0;
         int power = 1;
@@ -148,11 +170,11 @@ class Main extends Program {
     void test_intFromString(){
         assertTrue(123 == intFromString("123"));
     }
+    //Fonction "0102102500" => [1, 2, 10, 25, 0]
     int[] StringIntoInt(String numbers){
         int[] n = new int[length(numbers)/2];
         for(int i = 0; i<length(n); i++){
             n[i] = 10*(charAt(numbers, i*2)-'0')+(charAt(numbers, i*2+1)-'0');
-            println(n[i]);
         }
         return n;
     }
@@ -164,6 +186,34 @@ class Main extends Program {
         assertEquals(n[2], m[2]);
         assertEquals(n[3], m[3]);
     }
+
+    Item[] allItems(String file){
+        CSVFile f = loadCSV("csv/"+file);
+        Item[] inv = new Item[rowCount(f)-1];
+        for(int l = 0; l<length(inv);l++){
+            inv[l] = newItem(intFromString(getCell(f,l+1,0)), getCell(f,l+1,1), getCell(f,l+1,2));
+        }
+        return inv;
+    }
+    Item[] newInventaire(CSVFile save, int ligne){
+        Item[] inv = new Item[MAX_ITEMS];
+        int[] nombres = numberItemIntoIntTab(getCell(save,ligne,6));
+        for(int i = 0; i < length(inv); i++){
+            inv[i] = ALL_ITEMS[nombres[i]];
+        }
+        return inv;
+    }
+    int[] numberItemIntoIntTab(String numbers){
+        //numbers forcement de forme "1234"
+        int[] n = new int[MAX_ITEMS];
+        n[0] = charAt(numbers,0)-'0';
+        n[1] = charAt(numbers,1)-'0';
+        n[2] = charAt(numbers,2)-'0';
+        n[3] = charAt(numbers,3)-'0';
+        return n;
+    }
+    
+
 
 
     //Fonction pour sauvegarder dans le fichier de sauvegarde le joueur actuel (Joueur courant + autrs joueurs => save(String[][], sauvegardes.csv)
