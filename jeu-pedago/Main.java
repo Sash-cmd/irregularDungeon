4a405bc2c2b590e6bfec1c30b779b7e13d14c2e5
Jules Goddefroy <julesgoddefroy4c@gmail.com>
2025-12-09 21:23:21 +0100

    Tri_des_func_dans_des_groupes

--- jeu-pedago/Main.java
+++ jeu-pedago/Main.java
@@ -60,71 +60,18 @@ class Main extends Program {
 
         }
     }
-
-
     
     void SetUpGame(int numSauvegarde){
         joueurActuel = sauvegardes[numSauvegarde];
         positionJoueur = "Main Menu";
 
     }
+
     void afficherTxt(String file){
         File f = newFile(file);
         while(ready(f)){
             println(readLine(f));
         }
-    }
-
-    
-    boolean fightMonstre(Monstre m){
-        println("Un monstre se dresse devant vous !\n");
-        while(m.pv > 0 && joueurActuel.pv > 0){
-            println(toStringFight(m));
-            if(!askQuestion(m.verbe, m.color)){
-                //mauvaise réponse
-                damageJoueur();
-            }else{
-                //bonne réponse
-                damageMonstre(m, 40 + random(1,5));
-                changeVerb(m,1);
-            }
-        }
-        if(joueurActuel.pv == 0){
-            return false;
-        }else{
-            return true;
-        }
-    }
-    
-    boolean askQuestion(Verbe v, int color){
-        // retourne true si bonne réponse, false sinon.
-
-        //définition de quels mots sont visibles:
-        boolean[] affiche = new boolean[]{true, true, true, true};
-        if(color == 0){affiche[0] = false;}
-        if(color == 1 || color == 4 || color == 6 || color == 7){affiche[1] = false;}
-        if(color == 2 || color == 4 || color == 5 || color == 7){affiche[2] = false;}
-        if(color == 3 || color == 6 || color == 5 || color == 7){affiche[3] = false;}
-
-        //print de la question
-        if(affiche[0]){print(v.fr+" ; ");}else{print(" ______ ; ");}
-        if(affiche[1]){print(v.bv+" ; ");}else{print(" ______ ; ");}
-        if(affiche[2]){print(v.pr+" ; ");}else{print(" ______ ; ");}
-        if(affiche[3]){println(v.pp);}else{println(" ______");}
-        println("\nQuel est le verbe manquant ?");
-        println("\n"+toStringLittle(joueurActuel));
-
-        //definition 
-        String reponse = readInput();
-        int nbReponses = (color-1) / 3 + 1;
-    
-        if(color == 0){if(equals(reponse,v.fr)){return true;}else{return false;}}
-
-        if(color == 1 || color == 4 || color == 6 || color == 7){if(equals(reponse,v.bv)){return true;}else{return false;}}
-        if(color == 2 || color == 4 || color == 5 || color == 7){if(equals(reponse,v.pr)){return true;}else{return false;}}
-        if(color == 3 || color == 6 || color == 5 || color == 7){if(equals(reponse,v.pp)){return true;}else{return false;}}
-        
-        return false;
     }
 
     String readInput(){print(">>>");return readInput(readString());}
@@ -220,12 +167,70 @@ class Main extends Program {
     }   
     //-----------------------------------------------------//
     
+
+    
+    //-----------------/Fonctions de combat\---------------//
+
+    boolean fightMonstre(Monstre m){
+        println("Un monstre se dresse devant vous !\n");
+        while(m.pv > 0 && joueurActuel.pv > 0){
+            println(toStringFight(m));
+            if(!askQuestion(m.verbe, m.color)){
+                //mauvaise réponse
+                damageJoueur();
+            }else{
+                //bonne réponse
+                damageMonstre(m, 40 + random(1,5));
+                changeVerb(m,1);
+            }
+        }
+        if(joueurActuel.pv == 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    boolean askQuestion(Verbe v, int color){
+        // retourne true si bonne réponse, false sinon.
+
+        //définition de quels mots sont visibles:
+        boolean[] affiche = new boolean[]{true, true, true, true};
+        if(color == 0){affiche[0] = false;}
+        if(color == 1 || color == 4 || color == 6 || color == 7){affiche[1] = false;}
+        if(color == 2 || color == 4 || color == 5 || color == 7){affiche[2] = false;}
+        if(color == 3 || color == 6 || color == 5 || color == 7){affiche[3] = false;}
+
+        //print de la question
+        if(affiche[0]){print(v.fr+" ; ");}else{print(" ______ ; ");}
+        if(affiche[1]){print(v.bv+" ; ");}else{print(" ______ ; ");}
+        if(affiche[2]){print(v.pr+" ; ");}else{print(" ______ ; ");}
+        if(affiche[3]){println(v.pp);}else{println(" ______");}
+        println("\nQuel est le verbe manquant ?");
+        println("\n"+toStringLittle(joueurActuel));
+
+        //definition 
+        String reponse = readInput();
+        int nbReponses = (color-1) / 3 + 1;
+    
+        if(color == 0){                                          if(equals(reponse,v.fr)){return true;}else{return false;}}
+
+        if(color == 1 || color == 4 || color == 6 || color == 7){if(equals(reponse,v.bv)){return true;}else{return false;}}
+        if(color == 2 || color == 4 || color == 5 || color == 7){if(equals(reponse,v.pr)){return true;}else{return false;}}
+        if(color == 3 || color == 6 || color == 5 || color == 7){if(equals(reponse,v.pp)){return true;}else{return false;}}
+        
+        return false;
+    }
+
     //Il faut faire un truc qui change le verbe du monstre (+sa couleur si on veut) si il lui reste des PV.
     //Peut être un type de monstre qui peut en plus changer de couleur.
     void changeVerb(Monstre m, int level){
         m.verbe = generateVerbe(1);
     }
     
+
+    //-----------------------------------------------------//
+
     //Fonction pour transformer le csv en plusieurs Tableaux de verbes selon les differents niveaux
 
     //Fonction d'ériture pour avoir les caractères print les uns après les autres comme dans un RPG
